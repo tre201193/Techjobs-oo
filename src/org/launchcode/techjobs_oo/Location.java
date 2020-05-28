5a7cc778d492790f3fa3c99b41399425db28b8fd
tre201193 <61996174+tre201193@users.noreply.github.com>
2020-05-28 13:32:22 -0500

    finished PositionType Class

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -15,7 +15,10 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
-
+    public Location(String aValue) {
+        this();
+        this.value = aValue;
+    }
     // Custom toString, equals, and hashCode methods:
 
     @Override
